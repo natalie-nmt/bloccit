15afeb1683af485f7c9a05d4cf37b74f92be0a9f
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-01-27 20:39:56 -0500

    Added marco variable

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
     describe("GET /", () => {
@@ -16,14 +17,13 @@ describe("routes : static", () => {
 describe("routes : static", () => {
     describe("GET /marco", () => {
         it("should return status code 200", (done) => {
-            request.get(base, (err, res, body) => {
+            request.get(marco, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
             done();
             });
         });
         it("should contain polo in the body of the response", (done) => {
-            request.get(base, (err, res, body) => {
-                console.log(body.search("polo"));
+            request.get(marco, (err, res, body) => {
                 expect(body.search("polo")).not.toBe(-1);
             done();
             });
