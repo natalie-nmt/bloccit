2e55827ffdc8640193d5574701ad792a6066c09c
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-01-27 20:16:42 -0500

    Added /marco route and polo test

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
+    res.send("Welcome to Bloccit!");
+});
+
+router.get("/marco", (req, res, next) => {
+    res.send("polo");
 });
 
 module.exports = router;
