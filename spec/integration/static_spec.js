0fa1980ebb9a37af224ca515207012960dea7469
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-02-03 17:22:46 -0500

    Created tables for Topics and Banners

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,11 +6,11 @@ const about = "http://localhost:3000/about";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-        //done();
+        done();
       });
     });
 
@@ -19,11 +19,11 @@ describe("routes : static", () => {
 
 describe("routes : static", () => {
   describe("GET /about", () => {
-    it("should return status code 200 and have 'About Us' in the body of the response", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
       request.get(about, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("About Us");
-        //done();
+        expect(body).toContain("About Bloccit");
+        done();
       });
     });
 
