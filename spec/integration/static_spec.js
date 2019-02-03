2dc34b825017d989ea07c0f0f65433d3cde4b27e
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-02-03 13:51:42 -0500

    Added routing and tests for about us page

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,22 +1,29 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const about = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
-
-//#2
-it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
-
-//#3
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
+        //done();
+      });
+    });
 
-//#4
-        done();
+  });
+});
+
+describe("routes : static", () => {
+  describe("GET /about", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", () => {
+      request.get(about, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        //done();
       });
     });
 
