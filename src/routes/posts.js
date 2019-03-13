0ff32ab35d804cb1a95cff29cd1ad52bb2972f4f
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-03-12 20:35:01 -0400

    Fixed routing issue where validatePosts was being called on unnecessary CRUD actions

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,11 +4,11 @@ const postController = require("../controllers/postController");
 const validation = require("./validation");
 const helper = require("../auth/helpers");
 
-router.get("/topics/:topicId/posts/new", helper.ensureAuthenticated, validation.validatePosts, postController.new);
+router.get("/topics/:topicId/posts/new", helper.ensureAuthenticated, postController.new);
 router.post("/topics/:topicId/posts/create", helper.ensureAuthenticated, validation.validatePosts, postController.create);
 router.get("/topics/:topicId/posts/:id", postController.show);
-router.get("/topics/:topicId/posts/:id/edit", helper.ensureAuthenticated, validation.validatePosts, postController.edit);
+router.get("/topics/:topicId/posts/:id/edit", helper.ensureAuthenticated, postController.edit);
 router.post("/topics/:topicId/posts/:id/update", helper.ensureAuthenticated, validation.validatePosts, postController.update);
-router.post("/topics/:topicId/posts/:id/destroy", helper.ensureAuthenticated, validation.validatePosts, postController.destroy);
+router.post("/topics/:topicId/posts/:id/destroy", helper.ensureAuthenticated, postController.destroy);
 
 module.exports = router;
