db9ad1f2f86b99ee8c83445f4c7146412b6c0696
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-03-24 20:26:18 -0400

    In-progress—implemeting favoriting functionality and tests, working on fixing two test errors

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -35,6 +35,16 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "votes"
     });
+    Post.hasMany(models.Favorite, {
+      foreignKey: "postId",
+      as: "favorites"
+    });
+    Post.afterCreate((post, callback) => {
+      return models.Favorite.create({
+        userId: post.userId,
+        postId: post.id
+      });
+    });
   };
 
   Post.prototype.getPoints = function () {
@@ -45,5 +55,10 @@ module.exports = (sequelize, DataTypes) => {
       .map((v) => { return v.value })
       .reduce((prev, next) => { return prev + next });
   };
+
+  Post.prototype.getFavoriteFor = function(userId){
+    return this.favorites.find((favorite) => { return favorite.userId == userId });
+  };
+
   return Post;
 };
